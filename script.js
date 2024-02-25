25a23ba7dfe7dfde9d60f966578e3ae644176989
Eduardo <cojr.eduardo@gmail.com>
2024-02-25 14:22:06 +0100

    created showallgryffindor function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const addBtnGryffindor = document.getElementById("add-btn-gryffindor");
 
 let hogwartsStudents;
 
+//Read
 async function fetchStudent() {
   const hogwartsStudentRequest = await fetch(
     "https://hp-api.onrender.com/api/characters/students"
@@ -18,10 +19,22 @@ console.log(fetchStudent());
 async function fetchAndShowStudent() {
   try {
     hogwartsStudents = await fetchStudent();
-    console.log("inne i fetcandshowstudent", hogwartsStudents);
+    showAllGryffindor();
+    //console.log("Inside fetchAndShowStudent function", hogwartsStudents);
   } catch {
     console.log("unable to load Hogwarts Student", error);
   }
 }
 
 fetchAndShowStudent();
+
+//Read to show gryffindorstudents
+function showAllGryffindor() {
+  hogwartsStudents.forEach((gryffindorMember, index) => {
+    const gryffindorCard = document.createElement("div");
+
+    gryffindorCard.innerHTML = `<img src="${gryffindorMember.image}" style="width: 100px"/> <h3>${gryffindorMember.name}</h3>`;
+    gryffindorContainer.append(gryffindorCard);
+    console.log(gryffindorMember.gender);
+  });
+}
